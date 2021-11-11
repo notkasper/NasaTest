8b015ee48c7bf1b281e04d00cf9f6a315842b074
Kasper Karelse <kasperkarelse98@gmail.com>
2021-11-11 19:27:03 +0100

    raw + semi refined ingestion

--- ingest.js
+++ ingest.js
@@ -1,15 +1,23 @@
-const { parse: toCsv } = require("json2csv");
-const request = require("superagent");
-const path = require("path");
 const fs = require("fs");
+const path = require("path");
+const uuid = require("uuid");
+const request = require("superagent");
 const csvToJson = require("csvToJson");
+const { parse: toCsv } = require("json2csv");
+
+const RATE_LIMIT = 50; // Allowed amount of calls per minute
+const OUTPUT_BASE_DIR = path.join(__dirname, "./output");
+const INGEST_OUTPUT = `${OUTPUT_BASE_DIR}/csvPerCountry`;
+const TEMP_PATH = `${OUTPUT_BASE_DIR}/temp`;
+const START_DATE = "20210101";
+const END_DATE = "20210103";
 
 const sortByCountry = (jsonZipcodes) => {
   const locationsPerCountry = {};
 
   for (const index in jsonZipcodes) {
     const row = jsonZipcodes[index];
-    const { zipcode, city, country, latitude, longitude } = row;
+    const { country } = row;
 
     if (!locationsPerCountry[country]) {
       locationsPerCountry[country] = [];
@@ -20,31 +28,58 @@ const sortByCountry = (jsonZipcodes) => {
   return locationsPerCountry;
 };
 
-const getLocation = async (latitude, longitude) => {
+// const transformResponse = (response, parameters) => {
+//     const parameterResponses = response.body.properties.parameter
+//     const dates = parameterResponses[parameters[0]] // Bit ugly, but now we can loo
+//     return {}
+// }
+
+const getLocation = async (location) => {
+  const { longitude, latitude, zipcode, country, city } = location;
   const baseUrl = "https://power.larc.nasa.gov/api/temporal/daily/point";
+  const parameters = [
+    "T2M",
+    "PRECTOTCORR",
+    "RH2M",
+    "GWETROOT",
+    "GWETPROF",
+    "T2MWET",
+  ];
   const query = {
-    start: "20010101",
-    end: "20210101",
+    start: START_DATE,
+    end: END_DATE,
     community: "AG",
-    parameters: "T2M,PRECTOTCORR",
+    parameters: parameters.join(","),
     longitude,
     latitude,
+    format: "CSV",
   };
 
   console.info(`Awaiting response...`);
-  const res = await request.get(baseUrl).query(query);
+  const response = await request.get(baseUrl).query(query);
   console.info("Response received");
-  return res.body.properties.parameter;
+  const csvData = response.text.split("-END HEADER-")[1].trim(); // Remove meta data located above headers
+
+  const tempFilename = `${country}-${zipcode}-${city}-${START_DATE}-${END_DATE}-RAW`;
+  const tempFilePath = `${TEMP_PATH}/${country}/${tempFilename}.csv`;
+  await fs.promises.writeFile(tempFilePath, csvData);
+
+  const toJson = csvToJson();
+  let csvJson = await toJson.fromFile(tempFilePath);
+  const enrichedResponse = csvJson.map((row) => ({
+    ...row,
+    longitude,
+    latitude,
+    zipcode,
+    country,
+    city,
+  }));
+  const filename = `${country}-${zipcode}-${city}-${START_DATE}-${END_DATE}`;
+  const filepath = `${INGEST_OUTPUT}/${country}/${filename}.csv`;
+  await saveCountry(enrichedResponse, filepath);
 };
 
-const getBatch = async (locationBatch) => {
-  const responses = Promise.all(
-    locationBatch.map((location) =>
-      getLocation(location.latitude, location.longitude)
-    )
-  );
-  return responses;
-};
+const getBatch = (locationBatch) => Promise.all(locationBatch.map(getLocation));
 
 const wait = async (timeout) => {
   return new Promise((resolve) => {
@@ -64,56 +99,68 @@ const batchify = (arr, batchSize) => {
 };
 
 const ingestCountry = async (country, locations) => {
-  const responses = [];
-  const locationBatches = batchify(locations);
+  const locationBatches = batchify(locations, RATE_LIMIT);
+
+  // make sure country folder exists
+  if (!fs.existsSync(`${INGEST_OUTPUT}/${country}`)) {
+    fs.mkdirSync(`${INGEST_OUTPUT}/${country}`);
+  }
+
+  // make sure country folder exists for raw output
+  if (!fs.existsSync(`${TEMP_PATH}/${country}`)) {
+    fs.mkdirSync(`${TEMP_PATH}/${country}`);
+  }
+
   for (const locationBatch of locationBatches) {
     const startTime = new Date(); // keep track of time to avoid rate limit
-    const batchResponses = await getBatch(locationBatch);
-    responses.concat(batchResponses);
+    await getBatch(locationBatch);
     const endTime = new Date();
-    const timeToWaitMs = endTime.getTime() - startTime.getTime();
+    const elapsedTime = endTime.getTime() - startTime.getTime();
+    const timeToWaitMs = RATE_LIMIT - elapsedTime;
+
     if (timeToWaitMs > 0) {
       console.log(`Gonna wait ${Math.ceil(timeToWaitMs / 1000)} seconds`);
       await wait(timeToWaitMs);
     }
   }
-  return responses;
 };
 
-const saveCountry = (data, filepath) => {
+const saveCountry = async (data, filepath) => {
   const fields = [
-    "dayOfYear",
+    "YEAR",
+    "DOY",
+    "T2M",
+    "PRECTOTCORR",
+    "RH2M",
+    "GWETROOT",
+    "GWETPROF",
+    "T2MWET",
     "zipcode",
     "city",
     "country",
     "latitude",
     "longitude",
-    "temperature",
-    "precipitation",
   ];
   const options = { fields };
 
   try {
     const csv = toCsv(data, options);
-    fs.writeFileSync(filepath, csv);
+    await fs.promises.writeFile(filepath, csv);
   } catch (err) {
     console.error(err);
   }
 };
 
-const ingestAll = async (locationsPerCountry, outputDir) => {
+const ingestAll = async (locationsPerCountry) => {
   const countries = Object.keys(locationsPerCountry);
   for (const country of countries) {
     const locations = locationsPerCountry[country];
-    const countryResponses = await ingestCountry(country, locations);
-    const filepath = `${outputDir}/${country}.csv`;
-    saveCountry(countryResponses, filepath);
+    await ingestCountry(country, locations);
   }
 };
 
 const start = async () => {
   //   settings
-  const outputDir = "./output";
   const sourceFile = path.join(__dirname, "./source.csv");
 
   //   Load zipcodes
@@ -123,13 +170,23 @@ const start = async () => {
   //   sort locations per country
   const locationsPerCountry = sortByCountry(jsonZipcodes);
 
-  //   make sure country folder exists
-  if (!fs.existsSync(outputDir)) {
-    fs.mkdirSync(outputDir);
+  //   make sure output folder exists
+  if (!fs.existsSync(OUTPUT_BASE_DIR)) {
+    fs.mkdirSync(OUTPUT_BASE_DIR);
+  }
+
+  // Place where we store individual API responses, to be processed later
+  if (!fs.existsSync(TEMP_PATH)) {
+    await fs.promises.mkdir(TEMP_PATH);
+  }
+
+  //   make sure output folder exists
+  if (!fs.existsSync(INGEST_OUTPUT)) {
+    fs.mkdirSync(INGEST_OUTPUT);
   }
 
   //   ingest all data
-  await ingestAll(locationsPerCountry, outputDir);
+  await ingestAll(locationsPerCountry);
 };
 
 start();
